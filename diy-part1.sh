38e3e1cac6e9fb67b502c808b58dc1e82a0a38e1
LimonChoms <46754160+LimonChoms@users.noreply.github.com>
2020-11-21 00:33:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add V2ray
-git clone https://github.com/kuoruan/luci-app-v2ray package/luci-app-v2ray
+git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
