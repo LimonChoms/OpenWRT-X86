948d98761a4b8a934c7288d3754ecc24b3a42f96
LimonChoms <46754160+LimonChoms@users.noreply.github.com>
2022-07-28 23:37:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,7 +26,7 @@
 # done
 
 # 添加OpenClash
-git clone https://github.com/vernesong/OpenClash package/OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash package/OpenClash
 
 # 添加luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
